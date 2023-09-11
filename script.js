503608846c5bda46b240832dade7d3a5bbf1e41b
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-09-11 14:42:46 +0600

    simple web workder implementation

---
+++ script.js
@@ -0,0 +1,40 @@
+const $calc = document.querySelector('.calc')
+const $bgChange = document.querySelector('.bg-change')
+const worker = new Worker('worker.js')
+const $console = document.querySelector(".console");
+
+
+$bgChange.addEventListener("click", () => {
+  const colors = ["blue", "green", "yellow", "brown", "violet"];
+  const randomIdx = Math.floor(Math.random() * colors.length);
+
+  document.body.style.backgroundColor = colors[randomIdx];
+});
+
+worker.onmessage = function(message) {
+  $console.innerHTML += `<p>${message.data}<p>`
+  console.log(message.data)
+}
+
+$calc.addEventListener('click', () => {
+  $console.innerHTML = ''
+  worker.postMessage('please do the expensive calculation')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ worker.js
@@ -0,0 +1,11 @@
+onmessage = function(message) {
+  postMessage('started expensive calculation')
+  let sum = 0;
+  for(let i = 0; i < 1000000000; i++) {
+    sum+=i;
+  }
+  postMessage(sum)
+  postMessage("finished expensive calculation");
+}
+
+
